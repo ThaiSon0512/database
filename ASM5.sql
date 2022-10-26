97bd599400e21be2d23611628c8ea1d9198fea21
ThaiSon0512 <109654001+ThaiSon0512@users.noreply.github.com>
2022-10-26 19:04:54 +0700

    Add files via upload

--- ASM5.sql
+++ ASM5.sql
@@ -24,3 +24,36 @@ union ALL
 select '09434343';
 select * from DIENTHOAI;
 select DIENTHOAI.Telephone from DIENTHOAI right join DANHBA on DIENTHOAI.Telephone = DANHBA.Telephone
+-- cau 4
+select * from DANHBA;
+select Telephone from DANHBA;
+
+-- cau 5
+select * from DANHBA order by Name asc;
+select Telephone from DANHBA where Name like 'Nguyen Van An';
+select Name from DANHBA where Birthday like '2009-12-12';
+
+-- cau 6
+select count(Telephone) as soluongdt from DANHBA;
+
+select * from DANHBA;
+select * from DANHBA where Telephone like '123456789';
+
+-- cau 7
+update 
+
+-- cau 8
+create index IX_HoTen on DANHBA(Name);
+create index IX_SoDienThoi on DANHBA(Telephone);
+
+create view View_SoDienThoai as 
+select Name,Telephone from DANHBA;
+
+create view View_SinhNhat as
+select Name,Birthday,Telephone from DANHBA;
+
+create procedure SP_Them_DanhBa @name nvarchar(50), @address nvarchar(100), @telephone varchar(100), @birhtday date, @Id int as
+insert into DANHBA(Name,Address,Telephone,Birthday,Id)
+values(@name,@address,@telephone,@birhtday,@Id);
+
+create procedure SP_Tim_DanhBa
